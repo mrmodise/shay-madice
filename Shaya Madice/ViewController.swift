0c0a6471216c0e512bf505bd25117daf2d650414
mrmodise <modisemorebodi@gmail.com>
2018-07-07 20:01:52 +0200

    Initial Commit

---
+++ Shaya Madice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shaya Madice
+//
+//  Created by Msanzi Coder on 2018/07/07.
+//  Copyright © 2018 Mr Modise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
