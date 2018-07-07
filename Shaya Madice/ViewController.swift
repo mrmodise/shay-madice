12b0fbaaca47c022d2c9caddcffecfb9e6337b9d
mrmodise <modisemorebodi@gmail.com>
2018-07-07 20:49:42 +0200

    adding rolling functionality configuration

--- Shaya Madice/ViewController.swift
+++ Shaya Madice/ViewController.swift
@@ -10,14 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftDice: UIImageView!
+    @IBOutlet weak var rightDice: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func rollDicePressed(_ sender: UIButton) {
+        
     }
 
 
