ab48f6e3cd23ec7e9784aa04c8921d6be26854e7
mrmodise <modisemorebodi@gmail.com>
2018-07-21 23:17:25 +0200

    added roll dice functionality

--- Shaya Madice/ViewController.swift
+++ Shaya Madice/ViewController.swift
@@ -9,13 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+ 
     @IBOutlet weak var leftDice: UIImageView!
     @IBOutlet weak var rightDice: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
     
     override func didReceiveMemoryWarning() {
@@ -23,10 +28,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func rollDicePressed(_ sender: UIButton) {
-        
+    @IBAction func rollaDice(_ sender: Any) {
+       updateDiceImages()
     }
-
-
+    
+    func updateDiceImages() {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        leftDice.image = UIImage(named: diceArray[randomDiceIndex1])
+        
+        rightDice.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
 }
 
